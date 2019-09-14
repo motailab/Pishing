af7eddce45e2065af252ef552b6f5816c2ddb741
motailab <motailab@gmail.com>
2019-09-14 15:24:38 +0600

    added visitorinfo model in admin site.

--- facebook/admin.py
+++ facebook/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Victim, Target
+from .models import Victim, Target, VisitorInfo
 
 # Register your models here.
 admin.site.register(Victim)
 admin.site.register(Target)
+admin.site.register(VisitorInfo)
